20d2c5217726ccd7be7b63707037f7ed87d7cc6b
vicentlozano <lozanodroid@gmail.com>
2024-06-26 12:45:34 +0200

    nuevos archivos

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,15 +1,31 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 5,
+      name: "",
+      formData: "",
+      confirmedName: "",
+      formName: "",
     };
   },
   methods: {
-    increment() {
-      this.counter++;
+    increment(num) {
+      this.counter += num;
     },
-    decrement() {
-      this.counter--;
+    decrement(num) {
+      this.counter -= num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName; // para acceder al objeto evente usamos target. Para acceder al valor usamos value.
+    },
+    submitForm(event) {
+      //event.preventDefault(); Esta seria la forma antigua con javascript. Con vue, despues de indicar el evento en html podemos porner un . seguido del modificador que queramos.
+      alert("Submited!");
+      this.formData = "";
+    },
+    confirmedAct(event) {
+      this.confirmedName = this.name;
+      this.formName = "";
     },
   },
 });

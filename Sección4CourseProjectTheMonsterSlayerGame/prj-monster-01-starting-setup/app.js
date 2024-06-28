b25eed43946f9d6719736230972df92cb3634383
vicentlozano <lozanodroid@gmail.com>
2024-06-28 10:21:25 +0200

    terminado

--- Sección4CourseProjectTheMonsterSlayerGame/prj-monster-01-starting-setup/app.js
+++ Sección4CourseProjectTheMonsterSlayerGame/prj-monster-01-starting-setup/app.js
@@ -10,6 +10,7 @@ const app = Vue.createApp({
       winnerPlayer: false,
       isDraw: false,
       countRounds: 0,
+      listAttacks: [],
     };
   },
   methods: {
@@ -17,18 +18,24 @@ const app = Vue.createApp({
       // Esta formula calcula un numero aleatorio entre 5 y 12 redondeado con floor.
       let damage = getRandomValue(5, 12);
       this.monsterHealth -= damage;
-      this.attackPlayer();
       this.countRounds++;
+      this.addLogMessage("Player", "Attack", damage);
+      //this.listAtacks.push(`Player attacks: ${damage}`);
+      this.attackPlayer();
     },
     attackPlayer() {
       let damage = getRandomValue(8, 15);
       this.playerHealth -= damage;
+      this.addLogMessage("Monster", "Attack", damage);
+      //this.listAtacks.push(`Monster attacks: ${damage}`);
     },
     specialAtackMonster() {
       let damage = getRandomValue(10, 25);
       this.monsterHealth -= damage;
-      this.attackPlayer();
       this.countRounds = 0;
+      this.addLogMessage("Player", "Special attack", damage);
+      //this.listAtacks.push(` Special Player attacks: ${damage}`);
+      this.attackPlayer();
     },
     healPlayer() {
       let heal = getRandomValue(8, 25);
@@ -37,6 +44,9 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += heal;
       }
+      this.addLogMessage("Player", "Heals", heal);
+      //this.listAtacks.push(`Player heals: ${heal}`);
+
       this.attackPlayer();
       this.countRounds++;
     },
@@ -46,19 +56,39 @@ const app = Vue.createApp({
     newPlay() {
       this.winnerMonster = false;
       this.winnerPlayer = false;
+      this.isDraw = false;
       this.playerHealth = 100;
       this.monsterHealth = 100;
       this.countRounds = 0;
+      this.listAttacks = [];
+    },
+    addLogMessage(who, what, value) {
+      // unshift es lo mismo que push pero lo situa al principio de la matriz
+      this.listAttacks.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
   computed: {
     monsterBarStyles() {
-      return { width: this.monsterHealth + "%", textAlign: "center" };
+      if (this.monsterHealth <= 0) {
+        this.monsterHealth = 0;
+        return { width: this.monsterHealth + "%" };
+      } else {
+        return { width: this.monsterHealth + "%" };
+      }
     },
     playerBarStyles() {
-      return {
-        width: this.playerHealth + "%",
-      };
+      if (this.playerHealth <= 0) {
+        this.playerHealth = 0;
+        return {
+          width: this.playerHealth + "%",
+        };
+      } else {
+        return { width: this.playerHealth + "%" };
+      }
     },
     isCountRounds() {
       return this.countRounds < 3;
@@ -72,7 +102,6 @@ const app = Vue.createApp({
       } else if (value <= 0) {
         // El jugador pierde, el monstruo gana
         this.winnerMonster = true;
-        this.playerHealth = 0;
       }
     },
 
@@ -82,7 +111,6 @@ const app = Vue.createApp({
         this.isDraw = true;
       } else if (value <= 0) {
         this.winnerPlayer = true;
-        this.monsterHealth = 0;
       }
     },
   },

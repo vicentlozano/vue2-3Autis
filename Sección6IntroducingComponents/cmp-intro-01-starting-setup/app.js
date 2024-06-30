0cdeab1e6d6712f0feed900ea509774234901f1e
vicentlozano <lozanodroid@gmail.com>
2024-06-30 21:51:54 +0200

    nuevos moduulos

--- Sección6IntroducingComponents/cmp-intro-01-starting-setup/app.js
+++ Sección6IntroducingComponents/cmp-intro-01-starting-setup/app.js
@@ -3,26 +3,53 @@ const app = Vue.createApp({
     return {
       friends: [
         {
+          id: "73591933H",
           name: "Manuel Lopez",
           phone: "694501662",
           email: "manuel@localhost.com",
-          show: false,
         },
         {
+          id: "73591233H",
           name: "Julia Jones",
           phone: "694333333",
           email: "julia@localhost.com",
-          show: false,
         },
       ],
     };
   },
-  methods: {
-    showOrNot(index) {
-      this.friends[index].show.value = !this.friends[index].show.value;
-    },
-  },
+  methods: {},
   computed: {},
   watch: {},
 });
+//! Los componentes deben de nombrarse con dos palabra y guion para evitar conflictos con las etiqetas html
+app.component("friend-contact", {
+  template: ` 
+        <li>
+          <h2>{{friend.name}}</h2>
+          <button @click="toggleDetails()">
+           {{detailsAreVisible? 'Hide' : 'Show'}} Details
+          </button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{friend.phone}}</li>
+            <li><strong>Email:</strong> {{friend.email}}</li>
+          </ul>
+        </li>`,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "73591933H",
+        name: "Manuel Lopez",
+        phone: "694501662",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
 app.mount("#app");
+

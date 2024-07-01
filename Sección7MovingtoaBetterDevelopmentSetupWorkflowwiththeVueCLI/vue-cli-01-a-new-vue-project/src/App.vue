eca90c49d79f058465e56c2530fd166f0b981520
vicentlozano <lozanodroid@gmail.com>
2024-07-01 11:58:46 +0200

    nuevos modulos

---
+++ Sección7MovingtoaBetterDevelopmentSetupWorkflowwiththeVueCLI/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :friend="friend"
+      >
+      </friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+
+export default {
+  components: {
+    FriendContact,
+  },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "654345543",
+          email: "lozanopepe@gmail.com",
+        },
+        {
+          id: "pepe",
+          name: "Pepe Lorenz",
+          phone: "654345543",
+          email: "lozanopepe@gmail.com",
+        },
+      ],
+    };
+  },
+  methods: {},
+  computed: {},
+};
+</script>
+
---
+++ Sección7MovingtoaBetterDevelopmentSetupWorkflowwiththeVueCLI/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="showDetailOrNot()">
+      {{ showDetails ? "Hide Details" : "Show Details" }}
+    </button>
+    <ul v-if="showDetails">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["friend"],
+  data() {
+    return {
+      showDetails: false,
+    };
+  },
+  methods: {
+    showDetailOrNot() {
+      this.showDetails = !this.showDetails;
+    },
+  },
+  computed: {},
+};
+</script>
+
+<style src="../styles.css"></style>

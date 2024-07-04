a2d273f07a73fbc476b42ddb3dc9006746971204
vicentlozano <lozanodroid@gmail.com>
2024-07-04 18:01:11 +0200

    origin master

---
+++ Sección12SendingHttpRequests/http-01-starting-setup/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <learning-survey></learning-survey>
+  <user-experiences></user-experiences>
+  <MyProblema></MyProblema>
+</template>
+
+<script>
+import LearningSurvey from './components/survey/LearningSurvey.vue';
+import UserExperiences from './components/survey/UserExperiences.vue';
+import MyProblema from './components/MyProblema.vue';
+
+export default {
+  components: {
+    LearningSurvey,
+    UserExperiences,
+    MyProblema,
+  },
+  // data() {
+  //   return {
+  //     savedSurveyResults: [],
+  //   };
+  // },
+  // methods: {
+  //   storeSurvey(surveyData) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
